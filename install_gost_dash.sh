de79224342726a2455a22348663eaf99a3197c70
Seele Vollerei <xiercloud@outlook.com>
2025-01-17 02:16:39 +0800

    Update install_gost_dash.sh

--- install_gost_dash.sh
+++ install_gost_dash.sh
@@ -9,9 +9,7 @@ fi
 # 定义变量
 PROJECT_DIR="/opt/gost_web"
 APP_PY_URL="https://raw.githubusercontent.com/xierlove/gost_dash/refs/heads/main/app.py"
-GOST_VERSION="v2.11.1"  # 你可以根据需要更改版本
-GOST_FILE="gost-linux-amd64-${GOST_VERSION}.gz"
-GOST_URL="https://github.com/ginuerzh/gost/releases/download/${GOST_VERSION}/${GOST_FILE}"
+GOST_URL="https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz"
 RULES_FILE="${PROJECT_DIR}/rules.json"
 SERVICE_FILE="/etc/systemd/system/gost_web.service"
 
